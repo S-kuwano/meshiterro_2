7c9550841b35f9ec9616e7d6103586bb8f722d65
S-kuwano <tukushi14@gmail.com>
2021-06-16 10:50:50 +0000

    [finish]4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '115880c955af474d40796950b0ff81f4710302231733e5fea29d4b6d7fe183cbe9a8b26ce905ba091bd8a2be061216a5005b393c7b91c106bf96c0d15de0ac35'
