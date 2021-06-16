63d64efa54773c035d130a9410628ebd6084ee05
S-kuwano <tukushi14@gmail.com>
2021-06-16 11:41:49 +0000

    [finish]4-6

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,5 @@
 class PostImagesController < ApplicationController
-  
+
   def new
     @post_image = PostImage.new
   end
@@ -7,9 +7,14 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path    
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
+
   end
+
 
   def index
     @post_images = PostImage.page(params[:page]).reverse_order
@@ -25,13 +30,13 @@ class PostImagesController < ApplicationController
     @post_image.destroy
     redirect_to post_images_path
   end
-  
-  
+
+
   # 投稿データのストロングパラメータ
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
-  end  
-  
+  end
+
 end
